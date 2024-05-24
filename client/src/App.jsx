7338ddca5914667a2d04d10d15f55802dcba40ea
hellen <jameshellen2241@gmail.com>
2024-05-24 11:08:52 +0300

    ignored the pycache and venv

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <p className='text-[blue]'>hello</p>
+        
+    </>
+  )
+}
+
+export default App
---
+++ server/contacta_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContactaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contacta_app'
